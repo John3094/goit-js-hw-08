662df535ffd48a8522f901b77373525d110a4eb4
Yevhenii Yakovenko <yakovenkob@gmail.com>
2022-09-25 15:19:27 +0300

    finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+
+const formValue = {};
+
+const LOCAL_STORAGE_KEY = "feedback-form-state";
+
+form.addEventListener("input", throttle(onStorageSet, 500));
+form.addEventListener('submit', onFormSubmit);
+
+
+function onStorageSet() {
+    formValue.email = form.email.value;
+    formValue.message = form.message.value;
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formValue));
+};
+
+function onStorageGet() {
+    const savedSettings = localStorage.getItem(LOCAL_STORAGE_KEY);
+    const parce = JSON.parse(savedSettings);
+    if (parce) {
+    form.email.value = parce.email;
+    form.message.value = parce.message;
+    }
+};
+onStorageGet();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    if (e.currentTarget.email.value === '' || e.currentTarget.message.value === '') {
+        alert('Заповни всі поля!');
+    } else {
+        localStorage.removeItem(LOCAL_STORAGE_KEY);
+    }
+    form.reset();
+};
